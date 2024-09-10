e393a2d4ceaf29a464a5f09ad1a8d83343a3ed0a
Tolik pwmz <pal3noa@yandex.ru>
2024-09-10 20:53:16 +0300

    added admin functionality, and also tested existing models

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from comments.models import Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('comment', 'content', 'pub_date_time', 'author_comment')
+    exclude = ('pub_date_time', 'author_comment')
+    list_per_page = 100
+
